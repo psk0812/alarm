c3881eec0d601ab7c03e246959e82733fbe6b5ad
mac_park <eumgil97@naver.com>
2023-09-02 14:44:12 +0900

    "화면 다시 바꿈"

--- Site_Controller.swift
+++ Site_Controller.swift
@@ -13,30 +13,32 @@ import WebKit
 class Web_Controller:UIViewController{
     
     @IBOutlet var youtube_webview: WKWebView!
-    
-    
     @IBOutlet var back_btn: UIBarButtonItem!
-    
     @IBOutlet var url_select_btn: UIBarButtonItem!
-    
     @IBOutlet var forward_btn: UIBarButtonItem!
-    
-    
-
     @IBOutlet var ampm_pick: ampm_PickerView!
-    
     @IBOutlet var hour_pick: hour_PickerView!
-    
     @IBOutlet var minutes_pick: minutes_PickerView!
+    
+    @IBOutlet var monday: UIButton!
+    @IBOutlet var tuesday: UIButton!
+    @IBOutlet var wednesday: UIButton!
+    @IBOutlet var thursday: UIButton!
+    @IBOutlet var friday: UIButton!
+    @IBOutlet var saturday: UIButton!
+    @IBOutlet var sunday: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // URL에서 콘텐츠 로드
-        if let url = URL(string: "https://www.youtube.com") {
-            let request = URLRequest(url: url)
-            youtube_webview.load(request)
-            
-        }
+//
+//        // URL에서 콘텐츠 로드
+//        if let url = URL(string: "https://www.youtube.com") {
+//            let request = URLRequest(url: url)
+//            youtube_webview.load(request)
+//
+//        }
         
         
     }
@@ -70,13 +72,13 @@ class Web_Controller:UIViewController{
     }
     
     
-    
+    //이전 페이지로 이동
     @IBAction func forward_btn_clicked(_ sender: Any) {
         if youtube_webview.canGoForward{
             youtube_webview.goForward()
         }
     }
-    
+        //다시 돌아가기
     @IBAction func back_btn_clicked(_ sender: Any) {
         if youtube_webview.canGoBack{
             youtube_webview.goBack()
