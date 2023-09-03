de74cbc1156c1d8835e678d48cb589e1e9e957b7
mac_park <eumgil97@naver.com>
2023-09-03 17:58:35 +0900

    피커뷰 고치고

--- youtube_alram_Controller.swift
+++ youtube_alram_Controller.swift
@@ -14,22 +14,23 @@ import WebKit
 class youtube_alram_Controller:UIViewController,UIApplicationDelegate,DataDelegate{
     //유튜브 데이터 받을 준비
     var window: UIWindow?
-
+    
     var recive_youtubeurl : URL?
     var recive_youtube_title : String?
     var youtube_siteVC : UIViewController?
     
-  
     
     
+    @IBOutlet var ampm_pick: ampm_PickerView!
     
-   
+    @IBOutlet var hour_pick: hour_PickerView!
+    
+    @IBOutlet var minutes_pick: minutes_PickerView!
+    
     @IBOutlet var back_btn: UIBarButtonItem!
     @IBOutlet var url_select_btn: UIBarButtonItem!
     @IBOutlet var forward_btn: UIBarButtonItem!
-    @IBOutlet var ampm_pick: ampm_PickerView!
-    @IBOutlet var hour_pick: hour_PickerView!
-    @IBOutlet var minutes_pick: minutes_PickerView!
+
     
     @IBOutlet var monday: UIButton!
     
@@ -43,20 +44,25 @@ class youtube_alram_Controller:UIViewController,UIApplicationDelegate,DataDelega
     @IBOutlet var youtube_title: UILabel!
     @IBOutlet var caution_label: UILabel!
     
-
+    
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.caution_label.numberOfLines=0
         self.caution_label.text="주의! \n \n 유튜브가 실행되려면 와이파이나 데이터가 계속 연결된 상태여야 합니다. 만약 와이파이가 접속되지 않았다면,기본음이 들립니다."
-        print("로드 완료")
+        
+        // 이미지를 생성하여 배경 뷰에 설정
   
-      
+        print("로드 완료")
+        
+        
         guard let vc = self.youtube_siteVC as? Site_ViewController else{
             return
         }
-       
         
-  
+        
+        
     }
     
     //빈공간 터치시 키보드 내려감
@@ -71,8 +77,8 @@ class youtube_alram_Controller:UIViewController,UIApplicationDelegate,DataDelega
         youtube_title.text = string
         print("Received URL: \(url)")
         print("Received string: \(string)")
-
-
+        
+        
     }
     
     //유튜브 사이트 데이터 받기
@@ -82,7 +88,7 @@ class youtube_alram_Controller:UIViewController,UIApplicationDelegate,DataDelega
         }
     }
     
-
+    
     @IBAction func monday_clicked(_ sender:UIButton) {
         color_chage(sender)
     }
@@ -110,6 +116,17 @@ class youtube_alram_Controller:UIViewController,UIApplicationDelegate,DataDelega
     @IBAction func sunday_clicked(_ sender: UIButton) {
         color_chage(sender)
     }
+    
+    
+    
+    
+    @IBAction func vibrate_clicked(_ sender: UIButton) {
+        color_chage(sender)
+    }
+    
+    
+    
+    
     
     
     
